3bb6d3dca861f51e80e2397d523ae44a075051e0
Maciej <maciejfec1996@gmail.com>
2021-11-16 22:22:17 +0000

    added a run method to run the game. Added an empty deal method

--- MyPanel.java
+++ MyPanel.java
@@ -8,8 +8,10 @@ public class MyPanel extends JPanel implements ActionListener {
 
     private int WIDTH;
     private int HEIGHT;
-    private Timer shuffleTimer;
+    private Timer timer;
     private Image bg;
+
+    private Deck deck;
 
     private Image deckImg1;
     private int deck1X;
@@ -32,6 +34,8 @@ public class MyPanel extends JPanel implements ActionListener {
         this.HEIGHT = HEIGHT;
         this.bg = new ImageIcon("img\\background.png").getImage();
 
+        this.deck = new Deck();
+
         this.deckImg1 = new ImageIcon("img\\cards\\back.png").getImage();
         this.deck1X = (this.WIDTH / 2) - (this.deckImg1.getWidth(null) / 2);
         this.deck1Y = (this.HEIGHT / 2) - (this.deckImg1.getHeight(null) / 2);
@@ -42,49 +46,55 @@ public class MyPanel extends JPanel implements ActionListener {
         this.deck2Y = this.deck1Y;
         this.deck2Moving = true;
 
-        this.xVel = 5;
-        this.yVel = 3;
-
         this.shuffleCount = 0;
 
-        this.shuffleTimer = new Timer(0, this);
-        this.shuffleTimer.start();
+        this.xVel = 5;
+        this.yVel = -1;
+
+        this.timer = new Timer(0, this);
     }
 
-    public void shuffle() {
+    public void run() {
 
-        /*for (int i=0; i<4; i++) {
-            if (this.deck2Moving) {
-                deck2X = deck2X + this.xVel;
-                if (deck2X >= (deck1X + deckImg2.getWidth(null))) {
-                    this.deck2Moving = false;
-                }
-            }
-        }
+        this.deck.shuffle();
+        this.timer.start();
+    }
 
-         */
+    private void shuffle() {
+        // Animates the shuffling of a deck of cards
 
         if (this.deck2Moving) {
-            this.deck2X = this.deck2X + this.xVel;
+            this.deck2X += this.xVel;
+            this.deck2Y += this.yVel;
             if (deck2X >= (deck1X + deckImg2.getWidth(null))) {
                 this.deck2Moving = false;
                 int tempX = this.deck2X;
+                int tempY = this.deck2Y;
                 this.deck2X = this.deck1X;
+                this.deck2Y = this.deck1Y;
                 this.deck1X = tempX;
+                this.deck1Y = tempY;
                 this.xVel *= -1;
+                this.yVel *= -1;
                 this.deck1Moving = true;
             }
         }
 
         if (this.deck1Moving) {
-            this.deck1X = this.deck1X + this.xVel;
+            this.deck1X += this.xVel;
+            this.deck1Y += this.yVel;
             if (this.deck1X <= this.deck2X) {
                 this.deck1Moving = false;
                 this.xVel *= -1;
+                this.yVel *= -1;
                 this.deck2Moving = true;
                 this.shuffleCount += 1;
             }
         }
+    }
+
+    private void deal() {
+
     }
 
     @Override
@@ -93,8 +103,11 @@ public class MyPanel extends JPanel implements ActionListener {
         super.paintComponent(g2D);
 
         g2D.drawImage(this.bg, 0, 0, null);
-        g2D.drawImage(this.deckImg1, this.deck1X, this.deck1Y, null);
-        g2D.drawImage(this.deckImg2, this.deck2X, this.deck2Y, null);
+
+        if (this.deck.getDeckSize() > 0) {
+            g2D.drawImage(this.deckImg1, this.deck1X, this.deck1Y, null);
+            g2D.drawImage(this.deckImg2, this.deck2X, this.deck2Y, null);
+        }
     }
 
     @Override
@@ -102,6 +115,10 @@ public class MyPanel extends JPanel implements ActionListener {
 
         if (this.shuffleCount < 7) {
             this.shuffle();
+        }
+
+        if (this.deck.getDeckSize() > 0) {
+            this.deal();
         }
         repaint();
     }
