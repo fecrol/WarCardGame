48356ef52a221a081c860a52d7a9ebb6b6208ab8
Maciej <maciejfec1996@gmail.com>
2021-11-16 15:11:50 +0000

    created variables for WIDTH and HEIGHT to pass as parameters to myPanel.

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         MyFrame myFrame = new MyFrame();
-        MyPanel myPanel = new MyPanel();
+
+        int WIDTH = myFrame.getWIDTH();
+        int HEIGHT = myFrame.getHEIGHT();
+
+        MyPanel myPanel = new MyPanel(WIDTH, HEIGHT);
 
         myFrame.add(myPanel);
 
