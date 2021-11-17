c4d6962acf57015d0d0e8f63758a96085a16724c
Maciej <maciejfec1996@gmail.com>
2021-11-17 10:51:22 +0000

    Initial commit

---
+++ Hand.java
@@ -0,0 +1,25 @@
+package com.warCardGame;
+
+import java.util.ArrayList;
+
+public class Hand {
+
+    private ArrayList<Card> hand;
+
+    public Hand() {
+
+        this.hand = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        this.hand.add(card);
+    }
+
+    public void discardCard(Card card) {
+        this.hand.remove(card);
+    }
+
+    public int getHandSize() {
+        return this.hand.size();
+    }
+}
