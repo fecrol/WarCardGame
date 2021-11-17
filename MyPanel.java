c1180d597e3fb315fcd474c791e6006511cf7c70
Maciej <maciejfec1996@gmail.com>
2021-11-17 16:16:04 +0000

    Added a mouse listener for mouse click events. Added logic for mouse click. Added a playing method to see if a card is currently in play. Began working on the play method which will handle the play of a card.

--- MyPanel.java
+++ MyPanel.java
@@ -3,8 +3,10 @@ package com.warCardGame;
 import javax.swing.*;;import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MyPanel extends JPanel implements ActionListener {
+public class MyPanel extends JPanel implements ActionListener, MouseListener {
 
     private int WIDTH;
     private int HEIGHT;
@@ -15,21 +17,18 @@ public class MyPanel extends JPanel implements ActionListener {
     private Hand playerHand;
     private Hand computerHand;
 
-    private Image deckImg1;
+    private Image deckImg;
     private int deck1X;
     private int deck1Y;
     private boolean deck1Moving;
 
-    private Image deckImg2;
     private int deck2X;
     private int deck2Y;
     private boolean deck2Moving;
 
-    private Image playerHandImg;
     private int playerHandX;
     private int playerHandY;
 
-    private Image computerHandImg;
     private int computerHandX;
     private int computerHandY;
 
@@ -39,8 +38,11 @@ public class MyPanel extends JPanel implements ActionListener {
 
     private int shuffleCount;
 
+    private boolean clicked;
+
     public MyPanel(int WIDTH, int HEIGHT) {
 
+        this.addMouseListener(this);
         this.WIDTH = WIDTH;
         this.HEIGHT = HEIGHT;
         this.timer = new Timer(0, this);
@@ -50,29 +52,28 @@ public class MyPanel extends JPanel implements ActionListener {
         this.playerHand = new Hand();
         this.computerHand = new Hand();
 
-        this.deckImg1 = new ImageIcon("img\\cards\\back.png").getImage();
-        this.deck1X = (this.WIDTH / 2) - (this.deckImg1.getWidth(null) / 2);
-        this.deck1Y = (this.HEIGHT / 2) - (this.deckImg1.getHeight(null) / 2);
+        this.deckImg = new ImageIcon("img\\cards\\back.png").getImage();
+        this.deck1X = (this.WIDTH / 2) - (this.deckImg.getWidth(null) / 2);
+        this.deck1Y = (this.HEIGHT / 2) - (this.deckImg.getHeight(null) / 2);
         this.deck1Moving = false;
 
-        this.deckImg2 = new ImageIcon("img\\cards\\back.png").getImage();
         this.deck2X = this.deck1X;
         this.deck2Y = this.deck1Y;
         this.deck2Moving = true;
 
-        this.playerHandImg = new ImageIcon("img\\cards\\back.png").getImage();
-        this.playerHandX = (this.WIDTH / 2) - (this.deckImg1.getWidth(null) / 2);
-        this.playerHandY = this.HEIGHT - this.playerHandImg.getHeight(null) - 50;
+        this.playerHandX = (this.WIDTH / 2) - (this.deckImg.getWidth(null) / 2);
+        this.playerHandY = this.HEIGHT - this.deckImg.getHeight(null) - 50;
 
-        this.computerHandImg = new ImageIcon("img\\cards\\back.png").getImage();
-        this.computerHandX = (this.WIDTH / 2) - (this.deckImg1.getWidth(null) / 2);
+        this.computerHandX = (this.WIDTH / 2) - (this.deckImg.getWidth(null) / 2);
         this.computerHandY = 50;
+
+        this.deckXVel = 10;
+        this.deckYVel = -1;
+        this.deckDealYVel = 15;
 
         this.shuffleCount = 0;
 
-        this.deckXVel = 5;
-        this.deckYVel = -1;
-        this.deckDealYVel = 9;
+        this.clicked = false;
     }
 
     private void shuffle() {
@@ -81,7 +82,7 @@ public class MyPanel extends JPanel implements ActionListener {
         if (this.deck2Moving) {
             this.deck2X += this.deckXVel;
             this.deck2Y += this.deckYVel;
-            if (deck2X >= (deck1X + deckImg2.getWidth(null))) {
+            if (deck2X >= (deck1X + deckImg.getWidth(null))) {
                 this.deck2Moving = false;
                 int tempX = this.deck2X;
                 int tempY = this.deck2Y;
@@ -127,12 +128,31 @@ public class MyPanel extends JPanel implements ActionListener {
 
     }
 
+    private void play() {
+
+        Card playerCard = this.playerHand.discardCard(this.playerHand.getHandSize() - 1);
+        System.out.println("removed");
+        this.clicked = false;
+    }
+
     private boolean shuffling() {
-        return this.shuffleCount < 7;
+        return this.shuffleCount < 10;
     }
 
     private boolean dealing() {
-        return this.deck.getDeckSize() > 0 && this.shuffleCount >= 7;
+        return this.deck.getDeckSize() > 0 && !this.shuffling();
+    }
+
+    private boolean playing() {
+        return !this.shuffling() && !this.dealing() && this.clicked;
+    }
+
+    private boolean mouseOverCard(MouseEvent e) {
+
+        if (e.getX() >= this.playerHandX && e.getX() <= this.playerHandX + this.deckImg.getWidth(null) && e.getY() >= this.playerHandY && e.getY() <= this.playerHandY + this.deckImg.getHeight(null)) {
+            return true;
+        }
+        return false;
     }
 
     public void run() {
@@ -148,26 +168,29 @@ public class MyPanel extends JPanel implements ActionListener {
         g2D.drawImage(this.bg, 0, 0, null);
 
         if (this.playerHand.getHandSize() > 0 && this.dealing()) {
-            g2D.drawImage(this.playerHandImg, this.playerHandX, this.playerHandY, null);
-            g2D.drawImage(this.computerHandImg, this.computerHandX, this.computerHandY, null);
+            g2D.drawImage(this.deckImg, this.playerHandX, this.playerHandY, null);
+            g2D.drawImage(this.deckImg, this.computerHandX, this.computerHandY, null);
         }
 
         if (this.playerHand.getHandSize() >= 2 && !this.dealing()) {
-            g2D.drawImage(this.playerHand.getCard(this.playerHand.getHandSize() - 1).getImg(), this.playerHandX, this.playerHandY, null);
-            g2D.drawImage(this.playerHand.getCard(this.playerHand.getHandSize() - 2).getImg(), this.playerHandX, this.playerHandY, null);
-            g2D.drawImage(this.computerHandImg, this.computerHandX, this.computerHandY, null);
-            g2D.drawImage(this.computerHandImg, this.computerHandX, this.computerHandY, null);
+            g2D.drawImage(this.deckImg, this.playerHandX, this.playerHandY, null);
+            g2D.drawImage(this.deckImg, this.playerHandX, this.playerHandY, null);
+            g2D.drawImage(this.deckImg, this.computerHandX, this.computerHandY, null);
+            g2D.drawImage(this.deckImg, this.computerHandX, this.computerHandY, null);
+        }
+        else if (this.playerHand.getHandSize() < 2 && this.playerHand.getHandSize() > 0 && !this.dealing()) {
+            g2D.drawImage(this.deckImg, this.playerHandX, this.playerHandY, null);
+            g2D.drawImage(this.deckImg, this.computerHandX, this.computerHandY, null);
         }
 
         if (this.deck.getDeckSize() > 0) {
-            g2D.drawImage(this.deckImg1, this.deck1X, this.deck1Y, null);
-            g2D.drawImage(this.deckImg2, this.deck2X, this.deck2Y, null);
+            g2D.drawImage(this.deckImg, this.deck1X, this.deck1Y, null);
+            g2D.drawImage(this.deckImg, this.deck2X, this.deck2Y, null);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (this.shuffling()) {
             this.shuffle();
         }
@@ -175,6 +198,37 @@ public class MyPanel extends JPanel implements ActionListener {
         if (this.dealing()) {
             this.deal();
         }
+
+        if (this.playing()) {
+            this.play();
+        }
         repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            if (!this.shuffling() && !this.dealing() && !this.playing()) {
+                if (this.mouseOverCard(e)) {
+                    this.clicked = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
