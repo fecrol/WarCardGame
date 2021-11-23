74dc920bdbf7412cf7c2655a4ef24a7898994ca8
Maciej <maciejfec1996@gmail.com>
2021-11-23 20:10:11 +0000

    Added hand, playedCard and wonCards variables as well as getters for these variables

--- Computer.java
+++
@@ -1,4 +0,0 @@
-package com.warCardGame;
-
-public class Computer {
-}
--- Player.java
+++ Player.java
@@ -1,4 +1,27 @@
 package com.warCardGame;
 
 public class Player {
+
+    private Hand hand;
+    private Hand playedCard;
+    private Hand wonCards;
+
+    public Player() {
+
+        this.hand = new Hand();
+        this.playedCard = new Hand();
+        this.wonCards = new Hand();
+    }
+
+    public Hand getHand() {
+        return this.hand;
+    }
+
+    public Hand getPlayedCard() {
+        return this.playedCard;
+    }
+
+    public Hand getWonCards() {
+        return this.wonCards;
+    }
 }
